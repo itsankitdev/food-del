1d430683abab9d07280c423f01b2051d4d092021
Ankit <ankitk098565@gmail.com>
2026-03-18 17:02:13 +0530

    Clean project without secrets

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://greatstack:fooddelproject@cluster0.5dn2mum.mongodb.net/food-del").then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
 
     // mongodb+srv://<db_username>:<db_password>@cluster0.5dn2mum.mongodb.net/?appName=Cluster0
 }
