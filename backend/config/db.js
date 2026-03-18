2ef89bdfc6bccaf354462d798bff3ca07e35d1d5
Ankit <ankitk098565@gmail.com>
2026-03-18 16:55:01 +0530

    Fixed MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://greatstack:DNBn7G4nnMbdBE4F@cluster0.5dn2mum.mongodb.net/food-del").then(()=>console.log("DB Connected"));
+    await mongoose.connect("mongodb+srv://greatstack:fooddelproject@cluster0.5dn2mum.mongodb.net/food-del").then(()=>console.log("DB Connected"));
+
+    // mongodb+srv://<db_username>:<db_password>@cluster0.5dn2mum.mongodb.net/?appName=Cluster0
 }
